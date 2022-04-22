bcd5c9e5430372d141c6bb8ce942673410578da7
朱雁梁 <m63981996@126.com>
2022-04-22 08:40:38 +0800

    push

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,13 +7,15 @@ module.exports = {
 			{ text: '基础', link: '/basics/' },
 			{ text: '字符串', link: '/string/' },
 			{ text: '数组', link: '/array/' },
-			{ text: '对象', link: '/object/' }
+			{ text: '对象', link: '/object/' },
+			{ text: '正则表达式', link: '/reg/' }
 		],
 		sidebar: {
 			'/basics/': ['basics.md'],
 			'/string/': ['string.md'],
 			'/array/': ['array.md', '01array.md'],
-			'/object/': ['object.md']
+			'/object/': ['object.md'],
+			'/reg/': ['regepx.md']
 		}
 	}
 }
